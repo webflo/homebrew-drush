8fd52bebbe6dd7eb251a8c32c994a0f47d09dafb
Florian Weber <florian.weber@ueberbit.de>
2022-02-16 13:26:39 +0100

    Fix syntax

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -1,18 +1,18 @@
 class DrushLauncher < Formula
-    desc "Small wrapper around Drush for your global $PATH"
-    homepage "https://github.com/drush-ops/drush-launcher"
-    url "https://github.com/drush-ops/drush-launcher/releases/download/0.10.0/drush.phar"
-    sha256 "d466e4268dcb2f3465feeb01518d2770c4be8ca533e010b678799bbd14dd90c5"
-    license "GPL-2.0-or-later"
-  
-    uses_from_macos "php"
-    depends_on "php" => :test
-  
-    def install
-      bin.install "drush.phar" => "drush"
-    end
-  
-    test do
-      system "#{bin}/drush", "--drush-launcher-version"
-    end
+  desc "Small wrapper around Drush for your global $PATH"
+  homepage "https://github.com/drush-ops/drush-launcher"
+  url "https://github.com/drush-ops/drush-launcher/releases/download/0.10.0/drush.phar"
+  sha256 "d466e4268dcb2f3465feeb01518d2770c4be8ca533e010b678799bbd14dd90c5"
+  license "GPL-2.0-or-later"
+
+  depends_on "php" => :test
+  uses_from_macos "php"
+
+  def install
+    bin.install "drush.phar" => "drush"
   end
+
+  test do
+    system "#{bin}/drush", "--drush-launcher-version"
+  end
+end
--- Formula/drush8.rb
+++ Formula/drush8.rb
@@ -1,18 +1,18 @@
 class Drush8 < Formula
-    desc "Drush 8 (for Drupal 7)"
-    homepage "https://github.com/drush-ops/drush"
-    url "https://github.com/drush-ops/drush/releases/download/8.4.10/drush.phar"
-    sha256 "c974a84c314bb230e262445fac8c6fd964d5747ce0c5946fb67dcf86de571789"
-    license "GPL-2.0-or-later"
+  desc "Command-line tool for Drupal. Drush 8 is for Drupal 7"
+  homepage "https://github.com/drush-ops/drush"
+  url "https://github.com/drush-ops/drush/releases/download/8.4.10/drush.phar"
+  sha256 "c974a84c314bb230e262445fac8c6fd964d5747ce0c5946fb67dcf86de571789"
+  license "GPL-2.0-or-later"
 
-    uses_from_macos "php"
-    depends_on "php" => :test
+  depends_on "php" => :test
+  uses_from_macos "php"
 
-    def install
-      bin.install "drush.phar" => "drush8"
-    end
-
-    test do
-      system "#{bin}/drush8", "--version"
-    end
+  def install
+    bin.install "drush.phar" => "drush8"
   end
+
+  test do
+    system "#{bin}/drush8", "--version"
+  end
+end
