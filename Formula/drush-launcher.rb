998471742d5a12ecc893d258c87ad95376bbe7d7
Florian Weber <florian.weber@ueberbit.de>
2022-02-16 13:36:28 +0100

    Update Formula/drush-launcher.rb

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -1,8 +1,8 @@
 class DrushLauncher < Formula
   desc "Small wrapper around Drush for your global $PATH"
   homepage "https://github.com/drush-ops/drush-launcher"
-  url "https://github.com/drush-ops/drush-launcher/releases/download/0.10.0/drush.phar"
-  sha256 "d466e4268dcb2f3465feeb01518d2770c4be8ca533e010b678799bbd14dd90c5"
+  url "https://github.com/drush-ops/drush-launcher/releases/download/0.10.1/drush.phar"
+  sha256 "c6e3e520a33ac7790ab37e5af0fd91fc1108f0830a3a5ebe5b7c36e8782bea30"
   license "GPL-2.0-or-later"
 
   depends_on "php" => :test
