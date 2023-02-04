7ce24f26398622126a5ed5ec975c33ad094fe80e
Florian Weber <florian.weber@ueberbit.de>
2023-02-04 22:23:16 +0100

    drush8 8.4.11

--- Formula/drush8.rb
+++ Formula/drush8.rb
@@ -1,8 +1,8 @@
 class Drush8 < Formula
   desc "Command-line tool for Drupal. Drush 8 is for Drupal 7"
   homepage "https://github.com/drush-ops/drush"
-  url "https://github.com/drush-ops/drush/releases/download/8.4.10/drush.phar"
-  sha256 "c974a84c314bb230e262445fac8c6fd964d5747ce0c5946fb67dcf86de571789"
+  url "https://github.com/drush-ops/drush/releases/download/8.4.11/drush.phar"
+  sha256 "e030f5e8f407199cc01b09a6fd635e67f7432a7950b2571bdf9024a6226ce6d4"
   license "GPL-2.0-or-later"
 
   depends_on "php" => :test
