d70f6dc56d36519cda31b15fed7c4a1c25deeb0d
Florian Weber <florian.weber@ueberbit.de>
2025-09-13 10:43:21 +0200

    drush-launcher 2.1.0

--- Formula/drush-launcher.rb
+++ Formula/drush-launcher.rb
@@ -1,8 +1,8 @@
 class DrushLauncher < Formula
   desc "Small wrapper around Drush for your global $PATH"
   homepage "https://github.com/webflo/drush-launcher"
-  url "https://github.com/webflo/drush-launcher/archive/refs/tags/2.0.0.tar.gz"
-  sha256 "cc744ea0f9bdecc21254c9780b00ff4e7b060f9a7584627360c3901de28cfb5c"
+  url "https://github.com/webflo/drush-launcher/archive/refs/tags/2.1.0.tar.gz"
+  sha256 "ed7cd145151ac7fe5f2b27ef1ec04f5a40a2d441e42135790ce12471cb8e8dd7"
   license "GPL-2.0-or-later"
 
   def install
